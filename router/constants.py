efc195fa37fb8c93909c3daa161cb74d82cc0514
Hassani Mohammed <mouhamedtec@gmail.com>
2025-06-11 13:46:44 +0000

    Add constants for reverse geocoding services

---
+++ router/constants.py
@@ -0,0 +1,18 @@
+# pylint: disable=C0301
+
+"""Useful constants.
+
+Constants for reverse geocoding services.
+
+This module defines default URLs for popular reverse geocoding APIs.
+
+Attributes:
+    NOMINATIM_DEFAULT_URL (str): Default base URL for the Nominatim OpenStreetMap geocoding service.
+    PHOTON_DEFAULT_URL (str): Default base URL for the Photon geocoding service.
+
+"""
+
+__author__ = "Hassani Mohammed"
+
+NOMINATIM_DEFAULT_URL = "https://nominatim.openstreetmap.org"
+PHOTON_DEFAULT_URL = "https://photon.komoot.io"
